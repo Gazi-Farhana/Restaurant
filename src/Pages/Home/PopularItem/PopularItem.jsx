294c72f22bae3ac2e509fdaada3d7b7507d59bf9
Gazi-Farhana <gazifarhana95@gmail.com>
2023-05-24 23:57:51 +0600

    data fetched for popular items

--- src/Pages/Home/PopularItem/PopularItem.jsx
+++ src/Pages/Home/PopularItem/PopularItem.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import Menu from '../../Shared/Menu/Menu';
 
 const PopularItem = () => {
+const [menu, setMenu] = useState([]);
+
+useEffect(() =>{
+    fetch('menu.json')
+    .then(res => res.json())
+    .then(data => {
+
+        const popularItems = data.filter(item=> item.category === 'popular')
+        setMenu(popularItems)
+    })
+},[])
+
     return (
-        <section>
+        <section className='mb-12'>
             <SectionTitle 
             heading="From Our Menu"
             subHeading="Popular Items"
             ></SectionTitle>
+            <div className='grid md:grid-cols-2 gap-10'>
+                {menu.map(item=> <Menu key={item._id} item={item}/>)}
+            </div>
         </section>
     );
 };
