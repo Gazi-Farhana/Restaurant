573afd400cdd7f322ee0386bfae6244dfca13a10
Gazi-Farhana <gazifarhana95@gmail.com>
2023-05-24 16:33:27 +0600

    popular item title added

---
+++ src/Pages/Home/PopularItem/PopularItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
+const PopularItem = () => {
+    return (
+        <section>
+            <SectionTitle 
+            heading="From Our Menu"
+            subHeading="Popular Items"
+            ></SectionTitle>
+        </section>
+    );
+};
+
+export default PopularItem;
