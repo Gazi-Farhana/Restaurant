a4c859d5139547cb3b6907099e0b43fb4db4f2fc
Gazi-Farhana <gazifarhana95@gmail.com>
2023-05-24 15:36:15 +0600

    category section added

---
+++ src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,48 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import img1 from '../../../../public/assets/images/slide1.jpg'
+import img2 from '../../../../public/assets/images/slide2.jpg'
+import img3 from '../../../../public/assets/images/slide3.jpg'
+import img4 from '../../../../public/assets/images/slide4.jpg'
+import img5 from '../../../../public/assets/images/slide5.jpg'
+
+const Category = () => {
+    return (
+        <Swiper
+            slidesPerView={4}
+            spaceBetween={30}
+            centeredSlides={true}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper mb-24"
+        >
+            <SwiperSlide>
+                <img src={img1} alt="" />
+                <h3 className="text-4xl text-center text-white -mt-16 uppercase">Salads</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={img2} alt="" />
+                <h3 className="text-4xl text-center text-white -mt-16 uppercase">pizzas</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={img3} alt="" />
+                <h3 className="text-4xl text-center text-white -mt-16 uppercase">Soups</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={img4} alt="" />
+                <h3 className="text-4xl text-center text-white -mt-16 uppercase">Deserts</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={img5} alt="" />
+                <h3 className="text-4xl text-center text-white -mt-16 uppercase">Salsa Salads</h3>
+            </SwiperSlide>
+        </Swiper>
+    );
+};
+
+export default Category;
